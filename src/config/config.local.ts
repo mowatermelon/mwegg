fa9e28eaa61d04f6f084f347930832556b1a0c8e
melonHero <neefoxmo@gmail.com>
2021-09-14 15:53:38 +0800

    chore: remove orm config

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -4,17 +4,6 @@ export type DefaultConfig = PowerPartial<EggAppConfig>;
 
 export default (): DefaultConfig => {
   const config = {} as DefaultConfig;
-
-  config.orm = {
-    type: 'mysql',
-    host: '127.0.0.1',
-    port: 3306,
-    username: 'root',
-    password: 'zaozaorun',
-    database: 'zaozaoliao',
-    synchronize: false,
-    logging: false,
-  };
   
   return config;
 };
--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -5,16 +5,5 @@ export type DefaultConfig = PowerPartial<EggAppConfig>;
 export default (): DefaultConfig => {
   const config = {} as DefaultConfig;
 
-  config.orm = {
-    type: 'mysql',
-    host: '',
-    port: 3306,
-    username: '',
-    password: '',
-    database: undefined,
-    synchronize: false,
-    logging: false,
-  };
-
   return config;
 };
